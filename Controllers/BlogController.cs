fae8f58fb236c6dd181b975ea32a9eb330ebe8dc
Lucas Benninger <lucasbenninger@protonmail.com>
2020-01-15 10:05:08 -0500

    BlogPost View

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotBlog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IPostRepository postRepository;
+
+        public BlogController(IPostRepository postRepository)
+        {
+            this.postRepository = postRepository;
+        }
+        public IActionResult BlogPost(int id)
+        {
+            var post = postRepository.GetPostById(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+    }
+}
