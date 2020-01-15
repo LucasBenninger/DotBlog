01630a4fa2a9cc0d286a258f3c9e94af454fb2b7
Lucas Benninger <lucasbenninger@protonmail.com>
2020-01-14 22:16:24 -0500

    Add Bulma, FontAwesome, home Controller, Post Model, initail Layout and views

---
+++ Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotBlog.Models
+{
+    public class Post
+    {
+        public int BlogId { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Content { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -16,6 +16,8 @@ namespace DotBlog
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +26,19 @@ namespace DotBlog
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+                app.UseRouting();
             }
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
